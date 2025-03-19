d49ce10765b354240d178cd46bfbcf9e2753ebf9
bwazik <bwazaik@outlook.com>
2025-03-19 03:37:13 +0200

    Fixes 3 and translations

--- resources/views/admin/activities/questions/modals.blade.php
+++ resources/views/admin/activities/questions/modals.blade.php
@@ -1,12 +1,12 @@
 <!-- Add Offcanvas -->
 <x-offcanvas offcanvasType="add" offcanvasTitle="{{ trans('main.addItem', ['item' => trans('admin/questions.question')]) }}" action="{{ route('admin.questions.insert', $quizId) }}">
-    <x-text-area context="offcanvas" name="question_text_ar" label="{{ trans('admin/questions.question_text_ar') }}" placeholder="{{ trans('admin/receipts.placeholder.question_text_ar') }}" maxlength=750 required/>
-    <x-text-area context="offcanvas" name="question_text_en" label="{{ trans('admin/questions.question_text_en') }}" placeholder="{{ trans('admin/receipts.placeholder.question_text_en') }}" maxlength=750 required/>
+    <x-text-area context="offcanvas" name="question_text_ar" label="{{ trans('admin/questions.question_text_ar') }}" placeholder="{{ trans('admin/questions.placeholders.question_text_ar') }}" maxlength=750 required/>
+    <x-text-area context="offcanvas" name="question_text_en" label="{{ trans('admin/questions.question_text_en') }}" placeholder="{{ trans('admin/questions.placeholders.question_text_en') }}" maxlength=750 required/>
 </x-offcanvas>
 <!-- Edit Offcanvas -->
 <x-offcanvas offcanvasType="edit" offcanvasTitle="{{ trans('main.editItem', ['item' => trans('admin/questions.question')]) }}" action="{{ route('admin.questions.update') }}" id>
-    <x-text-area context="offcanvas" name="question_text_ar" label="{{ trans('admin/questions.question_text_ar') }}" placeholder="{{ trans('admin/receipts.placeholder.question_text_ar') }}" maxlength=750 required/>
-    <x-text-area context="offcanvas" name="question_text_en" label="{{ trans('admin/questions.question_text_en') }}" placeholder="{{ trans('admin/receipts.placeholder.question_text_en') }}" maxlength=750 required/>
+    <x-text-area context="offcanvas" name="question_text_ar" label="{{ trans('admin/questions.question_text_ar') }}" placeholder="{{ trans('admin/questions.placeholders.question_text_ar') }}" maxlength=750 required/>
+    <x-text-area context="offcanvas" name="question_text_en" label="{{ trans('admin/questions.question_text_en') }}" placeholder="{{ trans('admin/questions.placeholders.question_text_en') }}" maxlength=750 required/>
 </x-offcanvas>
 <!-- Delete Modal -->
 <x-modal modalType="delete" modalTitle="{{ trans('main.deleteItem', ['item' => trans('admin/questions.question')]) }}"
